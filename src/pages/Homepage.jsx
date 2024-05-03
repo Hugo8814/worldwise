bc155ed139c2c063ed47afeec30176088239d101
Hugo Pereira <128017769+Hugo8814@users.noreply.github.com>
2024-05-03 22:52:23 +0100

    boom

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,10 +1,11 @@
 import { Link } from "react-router-dom";
+import PageNav from "./components/PageNav";
 
 function Homepage() {
   return (
     <div>
+      <PageNav />
       <h1>Homepage</h1>
-      <Link to="/pricing">Product</Link>
     </div>
   );
 }
