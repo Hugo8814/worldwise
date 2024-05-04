a105eae736fffb623c983d3da607cb41f9ee4a67
Hugo Pereira <128017769+Hugo8814@users.noreply.github.com>
2024-05-04 12:16:55 +0100

    long day

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,10 +1,12 @@
-import PageNav from "./components/PageNav";
+import { Link } from "react-router-dom";
+import PageNav from "../components/PageNav";
 
 function Homepage() {
   return (
     <div>
       <PageNav />
       <h1>Homepage</h1>
+      <Link to="/app">go to app</Link>
     </div>
   );
 }
