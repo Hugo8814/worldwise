93eaf0f0213589ccd78dbabe581c7b54cae34106
Hugo Pereira <128017769+Hugo8814@users.noreply.github.com>
2024-05-05 23:35:35 +0100

    okay

--- src/App.jsx
+++ src/App.jsx
@@ -4,17 +4,23 @@ import Pricing from "./pages/Pricing";
 import Homepage from "./pages/Homepage";
 import AppLayout from "./pages/AppLayout";
 import Login from "./pages/Login";
+import CityList from "./components/cityList";
 //import PageNotFound from "./pages/PageNotFound";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Homepage />} />
+        <Route index element={<Homepage />} />
         <Route path="product" element={<Product />} />
         <Route path="pricing" element={<Pricing />} />
         <Route path="/login" element={<Login />} />
-        <Route path="app" element={<AppLayout />} />
+        <Route path="app" element={<AppLayout />}>
+          <Route index element={<p>list</p>} />
+          <Route path="cities" element={<CityList />} />
+          <Route path="countries" element={<p>list of countries</p>} />
+          <Route path="form" element={<p>form</p>} />
+        </Route>
         {/* <Route path="*" element={<PageNotFound />} /> */}
       </Routes>
     </BrowserRouter>
---
+++ src/components/CityList.jsx
@@ -0,0 +1,7 @@
+import styles from "./cityList.module.css";
+
+function cityList() {
+  return <ul className={styles.cityList}>list</ul>;
+}
+
+export default cityList;
