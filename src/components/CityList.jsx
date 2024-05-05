d32a56bfdfac6446991ad889b419a78cc1698bc7
Hugo Pereira <128017769+Hugo8814@users.noreply.github.com>
2024-05-05 23:54:18 +0100

    as

--- src/components/CityList.jsx
+++ src/components/CityList.jsx
@@ -1,9 +1,16 @@
+import CityItem from "./CityItem";
 import styles from "./cityList.module.css";
 import Spinner from "./Spinner";
 
 function cityList({ cities, isLoading }) {
   if (isLoading) return <Spinner />;
-  return <ul className={styles.cityList}></ul>;
+  return (
+    <ul className={styles.cityList}>
+      {cities.map((city) => (
+        <CityItem key={city.id} />
+      ))}
+    </ul>
+  );
 }
 
 export default cityList;
