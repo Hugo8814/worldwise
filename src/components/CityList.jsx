07e7c7603bf569dc207c6550c68f65a1b73d5394
Hugo Pereira <128017769+Hugo8814@users.noreply.github.com>
2024-05-06 00:36:58 +0100

    done

--- src/components/CityList.jsx
+++ src/components/CityList.jsx
@@ -1,13 +1,16 @@
 import CityItem from "./CityItem";
 import styles from "./cityList.module.css";
 import Spinner from "./Spinner";
+import Message from "./Message";
 
 function cityList({ cities, isLoading }) {
   if (isLoading) return <Spinner />;
+
+  if (!cities.length) return <Message message="add your first city" />;
   return (
     <ul className={styles.cityList}>
       {cities.map((city) => (
-        <CityItem key={city.id} />
+        <CityItem key={city.id} city={city} />
       ))}
     </ul>
   );
