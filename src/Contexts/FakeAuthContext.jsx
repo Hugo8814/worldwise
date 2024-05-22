b487d193095d0bd6ca9b3a2affa9ac92363b9db3
Hugo Pereira <128017769+Hugo8814@users.noreply.github.com>
2024-05-22 18:40:07 +0100

    ojat

--- src/Contexts/FakeAuthContext.jsx
+++ src/Contexts/FakeAuthContext.jsx
@@ -1,8 +1,37 @@
-import { createContext, useContext } from "react";
+import { createContext, useContext, useReducer } from "react";
 
 const AuthContext = createContext();
 
+const initialState = {
+  user: null,
+  isAuthenticated: false,
+};
+
+function reducer(state, action) {
+  switch (action.type) {
+    case "login":
+      return {
+        ...state,
+        isAuthenticated: true,
+        user: action.payload,
+      };
+    case "logout":
+      return {
+        ...state,
+        isAuthenticated: false,
+        user: null,
+      };
+    default:
+      throw new Error("Invalid action");
+  }
+}
+
 function AuthProvider({ children }) {
+  const [{ user, isAuthenticated }, dispatch] = useReducer(
+    reducer,
+    initialState
+  );
+
   function login(email, password) {}
   function logout() {}
 
