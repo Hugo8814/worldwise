e349bb3b00c49ce3aebd1c7629514a2dcf2e1573
Hugo Pereira <128017769+Hugo8814@users.noreply.github.com>
2024-05-22 20:11:14 +0100

    done

--- src/App.jsx
+++ src/App.jsx
@@ -13,6 +13,7 @@ import City from "./components/City";
 import Form from "./components/Form";
 import { CitiesProvider } from "./Contexts/CitiesContext";
 import { AuthProvider } from "./Contexts/FakeAuthContext";
+import ProtectedRoute from "./pages/ProtectedRoute";
 
 function App() {
   return (
@@ -25,7 +26,14 @@ function App() {
             <Route path="pricing" element={<Pricing />} />
 
             <Route path="login" element={<Login />} />
-            <Route path="app" element={<AppLayout />}>
+            <Route
+              path="app"
+              element={
+                <ProtectedRoute>
+                  <AppLayout />
+                </ProtectedRoute>
+              }
+            >
               <Route index element={<Navigate to="cities" replace={true} />} />
               <Route path="cities" element={<CityList />} />
               <Route path="cities/:id" element={<City />} />
--- src/Contexts/FakeAuthContext.jsx
+++ src/Contexts/FakeAuthContext.jsx
@@ -28,7 +28,7 @@ function reducer(state, action) {
 }
 
 const FAKE_USER = {
-  name: "Jack",
+  name: "hugo",
   email: "jack@example.com",
   password: "qwerty",
   avatar: "https://i.pravatar.cc/100?u=zz",
